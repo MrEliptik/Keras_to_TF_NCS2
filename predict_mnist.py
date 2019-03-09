0e4f6ec128839d4412b6d2059d742679c7591917
Victor Meunier <victormeunier.dev@gmail.com>
2019-03-09 13:07:45 +0100

    new model fp16 precision and new image

--- predict_mnist.py
+++ predict_mnist.py
@@ -1,55 +1,88 @@
-from mvnc import mvncapi as mvnc
-import numpy
 import cv2
+import sys
+import os
+import numpy as np
+import logging as log
+from time import time
+from openvino.inference_engine import IENetwork, IEPlugin
 from ImageProcessor import ImageProcessor
+
 processor = ImageProcessor()
-test_image = './data/photo_6.jpg'
+test_image = 'data/photo_6.jpg'
 input_image = cv2.imread(test_image)
 cropped_input, cropped = processor.preprocess_image(input_image)
-# Using NCS Predict
-# set the logging level for the NC API
-# mvnc.global_set_option(mvnc.GlobalOption.RW_LOG_LEVEL, 0)
 
-# get a list of names for all the devices plugged into the system
-devices = mvnc.enumerate_devices()
-if len(devices) == 0:
-    print('No devices found')
-    quit()
+perf_counts = False
 
-# get the first NCS device by its name.  For this program we will always open the first NCS device.
-dev = mvnc.Device(devices[0])
+def main():
+    log.basicConfig(format="[ %(levelname)s ] %(message)s", level=log.INFO, stream=sys.stdout)
+    model_xml = "IR_model/ir_model.xml"
+    model_bin = "IR_model/ir_model.bin"
 
-# try to open the device.  this will throw an exception if someone else has it open already
-try:
-    dev.open()
-except:
-    print("Error - Could not open NCS device.")
-    quit()
+    # Plugin initialization for specified device and load extensions library if specified
+    plugin = IEPlugin(device="MYRIAD")
 
-# Read a compiled network graph from file (set the graph_filepath correctly for your graph file)
-with open("graph", mode='rb') as f:
-    graphFileBuff = f.read()
+    # Read IR
+    log.info("Loading network files:\n\t{}\n\t{}".format(model_xml, model_bin))
+    net = IENetwork(model=model_xml, weights=model_bin)
 
-graph = mvnc.Graph('graph1')
+    assert len(net.inputs.keys())   == 1, "Sample supports only single input topologies"
+    assert len(net.outputs)         == 1, "Sample supports only single output topologies"
 
-# Allocate the graph on the device and create input and output Fifos
-in_fifo, out_fifo = graph.allocate_with_fifos(dev, graphFileBuff)
+    log.info("Preparing input blobs")
+    input_blob = next(iter(net.inputs))
+    out_blob = next(iter(net.outputs))
+    net.batch_size = len(test_image)
 
-# Write the input to the input_fifo buffer and queue an inference in one call
-graph.queue_inference_with_fifo_elem(in_fifo, out_fifo, cropped_input.astype('float32'), 'user object')
+    # Read and pre-process input images
+    n, c, h, w = net.inputs[input_blob].shape
+    images = np.ndarray(shape=(n, c, h, w))
 
-# Read the result to the output Fifo
-output, userobj = out_fifo.read_elem()
+    image = cv2.imread(test_image)
+    if image.shape[:-1] != (h, w):
+        log.warning("Image {} is resized from {} to {}".format(test_image, image.shape[:-1], (h, w)))
+        image = cv2.resize(image, (w, h))
+    image = image.transpose((2, 0, 1))  # Change data layout from HWC to CHW
 
-# Deallocate and destroy the fifo and graph handles, close the device, and destroy the device handle
-try:
-    in_fifo.destroy()
-    out_fifo.destroy()
-    graph.destroy()
-    dev.close()
-    dev.destroy()
-except:
-    print("Error - could not close/destroy Graph/NCS device.")
-    quit()
+    log.info("Batch size is {}".format(n))
 
-print("NCS \r\n", output, '\r\nPredicted:',output.argmax())
+    # Loading model to the plugin
+    log.info("Loading model to the plugin")
+    exec_net = plugin.load(network=net)
+    del net
+
+    # Start sync inference
+    log.info("Starting inference ({} iterations)".format(1))
+    infer_time = []
+    for i in range(1):
+        t0 = time()
+        res = exec_net.infer(inputs={input_blob: images})
+        infer_time.append((time()-t0)*1000)
+    log.info("Average running time of one iteration: {} ms".format(np.average(np.asarray(infer_time))))
+    if perf_counts:
+        perf_counts = exec_net.requests[0].get_perf_counts()
+        log.info("Performance counters:")
+        print("{:<70} {:<15} {:<15} {:<15} {:<10}".format('name', 'layer_type', 'exet_type', 'status', 'real_time, us'))
+        for layer, stats in perf_counts.items():
+            print("{:<70} {:<15} {:<15} {:<15} {:<10}".format(layer, stats['layer_type'], stats['exec_type'],
+                                                              stats['status'], stats['real_time']))
+
+    # Processing output blob
+    log.info("Processing output blob")
+    res = res[out_blob]
+    log.info("Top {} results: ".format(10))
+
+    for i, probs in enumerate(res):
+        probs = np.squeeze(probs)
+        top_ind = np.argsort(probs)[-10:][::-1]
+        print("Image {}\n".format(test_image))
+        for id in top_ind:
+            det_label = "#{}".format(id)
+            print("{:.7f} label {}".format(probs[id], det_label))
+        print("\n")
+
+    del exec_net
+    del plugin
+
+if __name__ == "__main__":
+    main()
